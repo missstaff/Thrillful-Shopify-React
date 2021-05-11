4617af391738a929566102b8674e268946905e3b
Georgia Fox <foxgc@my.lanecc.edu>
2021-05-11 12:59:45 -0700

    bolden collection titles

--- frontend/src/components/NavMenu.js
+++ frontend/src/components/NavMenu.js
@@ -1,4 +1,3 @@
-  
 import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 
@@ -30,22 +29,22 @@ const NavMenu = () => {
             <DrawerOverlay>
                 <DrawerContent>
                     <DrawerCloseButton />
-                    <DrawerHeader textAlign="center"><Link to="/collections" onClick={closeMenu}>Collections</Link></DrawerHeader>
+                    <DrawerHeader textAlign="center" color="#ff0000" _hover={{ color: "#000000"}}><Link to="/collections" style={{ fontWeight: "bold" }}onClick={closeMenu}>Collections</Link></DrawerHeader>
                     <DrawerBody backgroundColor="#000000" color="#ff0000" flexDirection="row" alignItems="center" justifyContent="space-between" >
                         <VStack _hover={{ color: "#ffffff"}} p="2rem">
-                            <Link to="/all-products" onClick={closeMenu}>All Products</Link>
+                            <Link to="/all-products" style={{ fontWeight: "bold" }}onClick={closeMenu}>All Products</Link>
                         </VStack>
                         <VStack _hover={{ color: "#ffffff" }} p="2rem">
-                            <Link to="/Z2lkOi8vc2hvcGlmeS9Db2xsZWN0aW9uLzI2NTc2MzM1Njg0MA==" onClick={closeMenu}>Dresses</Link>
+                            <Link to="/Z2lkOi8vc2hvcGlmeS9Db2xsZWN0aW9uLzI2NTc2MzM1Njg0MA==" style={{ fontWeight: "bold" }}onClick={closeMenu}>Dresses</Link>
                         </VStack>
                         <VStack _hover={{ color: "#ffffff" }} p="2rem">
-                            <Link to="/Z2lkOi8vc2hvcGlmeS9Db2xsZWN0aW9uLzI2NjQ5ODU3MjQ1Ng==" onClick={closeMenu}>Two-Piece Sets</Link>
+                            <Link to="/Z2lkOi8vc2hvcGlmeS9Db2xsZWN0aW9uLzI2NjQ5ODU3MjQ1Ng==" style={{ fontWeight: "bold" }}onClick={closeMenu}>Two-Piece Sets</Link>
                         </VStack>
                         <VStack _hover={{ color: "#ffffff" }} p="2rem">
-                            <Link to="/Z2lkOi8vc2hvcGlmeS9Db2xsZWN0aW9uLzI2NjQ5OTUyMjcyOA==" onClick={closeMenu}>Accessories</Link>
+                            <Link to="/Z2lkOi8vc2hvcGlmeS9Db2xsZWN0aW9uLzI2NjQ5OTUyMjcyOA==" style={{ fontWeight: "bold" }}onClick={closeMenu}>Accessories</Link>
                         </VStack>
                         <VStack _hover={{ color: "#ffffff" }} p="2rem">
-                            <Link to="/Z2lkOi8vc2hvcGlmeS9Db2xsZWN0aW9uLzI2NjQ5OTQ1NzE5Mg==" onClick={closeMenu}>Beauty</Link>
+                            <Link to="/Z2lkOi8vc2hvcGlmeS9Db2xsZWN0aW9uLzI2NjQ5OTQ1NzE5Mg==" style={{ fontWeight: "bold" }}onClick={closeMenu}>Beauty</Link>
                         </VStack>
                     </DrawerBody>
                     <DrawerFooter>
