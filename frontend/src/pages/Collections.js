20e28ecc9418b0fbd4257141f3620d0c1ee4fa63
Georgia Fox <foxgc@my.lanecc.edu>
2021-05-11 16:59:49 -0700

    add heading

--- frontend/src/pages/Collections.js
+++ frontend/src/pages/Collections.js
@@ -1,11 +1,11 @@
 import React, { useContext, useEffect } from 'react';
-import { Box, Grid, Image, Text, Center } from '@chakra-ui/react'
+import { Box, Grid, Heading, Image, Text, Center } from '@chakra-ui/react'
 import { ShopContext } from '../context/shopContext';
 import { Link } from 'react-router-dom'
 
 
 const Collections = () => {
-    
+
     const { fetchAllCollections, collections } = useContext(ShopContext)
 
 
@@ -19,19 +19,22 @@ const Collections = () => {
 
     return (
         <Box p="2rem">
+            <Center>
+                <Heading>All Collections</Heading>
+            </Center>
             <Grid templateColumns={["repeat(1, 1fr)", "repeat(3, 1fr)"]} m="auto">
                 {
                     collections.map(collection => (
                         <Link to={`/${collection.id}`} key={collection.id}>
                             <Center>
-                            <Box _hover={{ opacity: '80%' }} textAlign='center'/*move text here*/>
-                                <Image src={collection.image.src} p="2rem" h="25rem" w="25rem" /> 
-                               <Center>
-                               <Text style={{fontWeight:"bold"}}>
-                                    {collection.title}
-                                </Text>
-                               </Center>
-                            </Box>
+                                <Box _hover={{ opacity: '80%' }} textAlign='center'/*move text here*/>
+                                    <Image src={collection.image.src} p="2rem" h="25rem" w="25rem" />
+                                    <Center>
+                                        <Text style={{ fontWeight: "bold" }}>
+                                            {collection.title}
+                                        </Text>
+                                    </Center>
+                                </Box>
                             </Center>
                         </Link>
                     ))
