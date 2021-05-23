cdce93459c91ef3759c29ec29677ff932c6fbb6f
Shawna Staff <staffs@my.lanecc.edu>
2021-05-23 15:54:22 -0700

    Revert "Merge pull request #15 from TODO-Team-Name-Capstone/heroku"
    This reverts commit 71bf8d1d768f87051c31953ba6b7610f381aa6a8, reversing
    changes made to 646b1c9e6d2978845272ce88c3acaab5b96bcebe.

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import path from 'path';
 //import data from './data.js';
 import userRouter from './routers/userRouter.js';
 
@@ -25,16 +24,9 @@ mongoose.connect(process.env.MONGODB_URL, {
 
 app.use('/api/users', userRouter);
 
-const __dirname = path.resolve();
-
-app.use(express.static(path.join(__dirname, '/frontend/build')));
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
-);
-
-// app.get('/', (req, res) => {
-//   res.send('Server is ready');
-// });
+app.get('/', (req, res) => {
+  res.send('Server is ready');
+});
 
 app.use((err, req, res, next) => {
     res.status(500).send({ message: err.message });
