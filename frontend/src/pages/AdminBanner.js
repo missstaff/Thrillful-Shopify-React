4ef2ef0a718f099062eb9d1745636aac820adab0
QueenOfWisps <brittainr@my.lanecc.edu>
2021-05-27 14:30:18 -0700

    admin banner files restructured.

--- frontend/src/pages/AdminBanner.js
+++ frontend/src/pages/AdminBanner.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import '../css/banner.css';
 import { Box, Button, Input } from '@chakra-ui/react';
 //import axios from 'axios';
+import axios from 'axios';
 
 
 
@@ -9,7 +10,8 @@ import { Box, Button, Input } from '@chakra-ui/react';
 class AdminBanner extends Component {
     //initialize state 
     state = {
-        selectedFile: null
+        selectedFile: null,
+        bannerImg: ""
     }
 
     //file is now stored in the selected state
@@ -18,6 +20,22 @@ class AdminBanner extends Component {
             selectedFile: event.target.files[0]
         })
     }
+    textHandler = event => {
+        this.setState({
+            bannerImg: event.target.value
+        })
+    }
+
+    fetchUserDetails=()=>{
+        fetch(`http://localhost:5000/img`)
+           .then(res => res.json())
+           .then(data =>{
+            this.setState({bannerImg:data.items[data.items.length-1].img})
+           }).catch((err)=>console.log(err))
+    
+        }
+    
+    
 
     //method no argument goal is to upload a file. 
     //https://academind.com/tutorials/reactjs-image-upload/
@@ -37,10 +55,45 @@ class AdminBanner extends Component {
     //     })
     // }
 
-    render() {
+    fileUploadHandler=(e)=>{
+        e.preventDefault()
+
+        const formData=new FormData();
+        formData.append("image",this.state.selectedFile);
+        formData.append("text", this.state.bannerImg)
+
+       
+  fetch('http://localhost:5000/img/', {
+     method: 'POST',
+     body: formData
+   }).then(res => res.json())
+   .then(data => {
+     alert("Picture uploaded successfully");
+     window.location.reload();
+     console.log(data)
+   })
+   .catch((e)=>console.log(e))
+   
+}
+
+    componentDidMount(){
+        this.fetchUserDetails();
+       }
+   
+   render(){
+   
+       if(this.state.bannerImg){
+           var imagestr=this.state.bannerImg;
+           var profilePic= imagestr
+       }
+       
+
         return (
+
             <Box className="imageBox">
+                <img src={profilePic} alt="profils pic" style={{width: "80%", height: "300px"}} />
                 <Input type="file" onChange={this.fileSelectedHandler} />
+                <Input type="text" placeholder="write your text here..." onChange={this.textHandler} />
                 <Button onClick={this.fileUploadHandler}>Upload</Button>
 
             </Box>
