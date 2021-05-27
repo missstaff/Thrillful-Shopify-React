6389346f93f2657dd1c147733473c6f4a0864fcf
Shawna Staff <staffs@my.lanecc.edu>
2021-05-27 16:23:18 -0700

    Revert "Revert "admin banner files restructured.""
    This reverts commit f48deab8f9bb1fde2a17663dabbc7074d260f6e1.

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import dotenv from 'dotenv';
 import path from 'path';
 //import data from './data.js';
 import userRouter from './routers/userRouter.js';
+import imageRouter from './routers/imageRouter.js'
+import morgan from 'morgan';
 import contactRouter from './routers/contact.router.js';
 import cors from 'cors';
 
@@ -11,6 +13,7 @@ const port = process.env.PORT || 5000;
 
 dotenv.config();
 const app = express();
+app.use(morgan('dev'));
 app.use(express.json()); //Used to parse JSON bodies
 app.use(express.urlencoded({ extended: true })); //Parse URL-encoded bodies
 app.use(cors({ credentials: true, origin: 'http://localhost:3000'}));
@@ -28,6 +31,16 @@ mongoose.connect(process.env.MONGODB_URL, {
 
 app.use('/api/users', userRouter);
 app.use('/api/contact', contactRouter);
+
+//IMAGES
+app.use("/img", imageRouter)
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+},()=>console.log('connected to DB'))
+
+
 
 const __dirname = path.resolve();
 
